85fcd7c167fa841ebc9b125c01af78809751197a
fzlaafar <fzlaafar@agilorg.com>
2015-08-12 15:51:40 +0100

    up openerp

--- l10n_maroc_agilorg/__openerp__.py
+++ l10n_maroc_agilorg/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name' : 'Maroc - Accounting',
     'version' : '1.0',
-    'author' : 'kazacube',
+    'author' : 'Agilorg',
     'category' : 'Localization/Account Charts',
     'description': """
 This is the base module to manage the accounting chart for Maroc.
@@ -31,14 +31,13 @@ This is the base module to manage the accounting chart for Maroc.
 Ce Module charge le modèle du plan de comptes standard Marocain et permet de
 générer les états comptables aux normes marocaines (Bilan, CPC (comptes de
 produits et charges), balance générale à 6 colonnes, Grand livre cumulatif...).
-L'intégration comptable a été validé avec l'aide du Cabinet d'expertise comptable
-Seddik au cours du troisième trimestre 2010.""",
-    'website': 'http://www.kazacube.com',
+""",
+    'website': 'http://www.agilorg.com',
     'depends' : ['base', 'account'],
     'data' : [
         'security/ir.model.access.csv',
         'account_type.xml',
-        'account_normal_morocco.xml',
+        'Agilorg_Pcg_Normal.xml',
         'Agilorg_Pcg_Simplifié.xml',
         'l10n_ma_tax_simple.xml',
         'l10n_ma_wizard.xml',    
